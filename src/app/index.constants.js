5357462129847ca620a2b4c59cd328f05b4d9d47
Jack Scott <mast.j.scott@gmail.com>
2016-10-15 15:57:49 +0800

    initial boilerplate generated

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('healthhack')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
